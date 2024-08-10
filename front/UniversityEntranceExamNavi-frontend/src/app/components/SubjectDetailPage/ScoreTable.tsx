7a940788febcca998b0b5d245fe8c48bd1f5f3ce
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-08-10 13:31:58 +0900

    fix: 詳細ページの表のモバイルの表示崩れを修正

--- front/UniversityEntranceExamNavi-frontend/src/app/components/SubjectDetailPage/ScoreTable.tsx
+++ front/UniversityEntranceExamNavi-frontend/src/app/components/SubjectDetailPage/ScoreTable.tsx
@@ -8,53 +8,60 @@ const ScoreTable = ({
 }) => (
   <div className="mt-4">
     <h2 className="text-lg font-semibold mb-2">科目別配点と割合</h2>
-    <table className="min-w-full border-collapse border border-gray-300">
-      <thead>
-        <tr>
-          <th className="border border-gray-300 p-2">項目</th>
-          {Object.keys(subjectScores).map((subjectName) => (
-            <th key={subjectName} className="border border-gray-300 p-2">
-              {subjectName}
+    <div className="overflow-x-auto">
+      <table className="min-w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 p-2 whitespace-nowrap">
+              項目
             </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        <ScoreTableRow
-          label="共通テスト 配点"
-          keyName="commonTestScore"
-          scores={subjectScores}
-        />
-        <ScoreTableRow
-          label="配点合計に占める割合"
-          keyName="commonTestRatio"
-          scores={subjectScores}
-          isPercentage
-        />
-        <ScoreTableRow
-          label="二次試験 配点"
-          keyName="secondTestScore"
-          scores={subjectScores}
-        />
-        <ScoreTableRow
-          label="配点合計に占める割合"
-          keyName="secondTestRatio"
-          scores={subjectScores}
-          isPercentage
-        />
-        <ScoreTableRow
-          label="共通テスト + 二次試験 配点"
-          keyName="totalScore"
-          scores={subjectScores}
-        />
-        <ScoreTableRow
-          label="配点合計に占める割合"
-          keyName="totalRatio"
-          scores={subjectScores}
-          isPercentage
-        />
-      </tbody>
-    </table>
+            {Object.keys(subjectScores).map((subjectName) => (
+              <th
+                key={subjectName}
+                className="border border-gray-300 p-2 whitespace-nowrap"
+              >
+                {subjectName}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <ScoreTableRow
+            label="共通テスト 配点"
+            keyName="commonTestScore"
+            scores={subjectScores}
+          />
+          <ScoreTableRow
+            label="配点合計に占める割合"
+            keyName="commonTestRatio"
+            scores={subjectScores}
+            isPercentage
+          />
+          <ScoreTableRow
+            label="二次試験 配点"
+            keyName="secondTestScore"
+            scores={subjectScores}
+          />
+          <ScoreTableRow
+            label="配点合計に占める割合"
+            keyName="secondTestRatio"
+            scores={subjectScores}
+            isPercentage
+          />
+          <ScoreTableRow
+            label="共通テスト + 二次試験 配点"
+            keyName="totalScore"
+            scores={subjectScores}
+          />
+          <ScoreTableRow
+            label="配点合計に占める割合"
+            keyName="totalRatio"
+            scores={subjectScores}
+            isPercentage
+          />
+        </tbody>
+      </table>
+    </div>
   </div>
 );
 

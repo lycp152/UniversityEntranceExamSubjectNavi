73f29262ef706c58c40470b798b2ccd266069bb3
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-08-10 13:27:03 +0900

    fix: 並び替え条件のモバイルの表示崩れを修正

--- front/UniversityEntranceExamNavi-frontend/src/app/components/SearchResultTable.tsx
+++ front/UniversityEntranceExamNavi-frontend/src/app/components/SearchResultTable.tsx
@@ -22,42 +22,72 @@ const ResultTable: React.FC = () => {
   return (
     <div className="bg-white shadow p-4">
       <h2 className="text-xl font-bold mb-4">{title}</h2>
-      <table className="min-w-full bg-white border border-gray-300">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b text-left">順位</th>
-            <th className="py-2 px-4 border-b text-left">比率（%）</th>
-            <th className="py-2 px-4 border-b text-left">大学名</th>
-            <th className="py-2 px-4 border-b text-left">学部・募集枠</th>
-            <th className="py-2 px-4 border-b text-left">学科・専攻・募集枠</th>
-            <th className="py-2 px-4 border-b text-left">日程</th>
-            <th className="py-2 px-4 border-b text-left">募集人員</th>
-          </tr>
-        </thead>
-        <tbody>
-          {subjects.map((subject) => (
-            <tr
-              key={subject.subjectId}
-              className="cursor-pointer hover:bg-gray-100"
-              onClick={() =>
-                handleRowClick(
-                  subject.universityId,
-                  subject.departmentId,
-                  subject.subjectId
-                )
-              }
-            >
-              <td className="py-2 px-4 border-b">{subject.rank}</td>
-              <td className="py-2 px-4 border-b">{subject.subjectRatio} %</td>
-              <td className="py-2 px-4 border-b">{subject.universityName}</td>
-              <td className="py-2 px-4 border-b">{subject.department}</td>
-              <td className="py-2 px-4 border-b">{subject.major}</td>
-              <td className="py-2 px-4 border-b">{subject.schedule}</td>
-              <td className="py-2 px-4 border-b">{subject.enrollment} 名</td>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-300">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                順位
+              </th>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                比率（%）
+              </th>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                大学名
+              </th>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                学部・募集枠
+              </th>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                学科・専攻・募集枠
+              </th>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                日程
+              </th>
+              <th className="py-2 px-4 border-b text-left whitespace-nowrap">
+                募集人員
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {subjects.map((subject) => (
+              <tr
+                key={subject.subjectId}
+                className="cursor-pointer hover:bg-gray-100"
+                onClick={() =>
+                  handleRowClick(
+                    subject.universityId,
+                    subject.departmentId,
+                    subject.subjectId
+                  )
+                }
+              >
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.rank}
+                </td>
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.subjectRatio} %
+                </td>
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.universityName}
+                </td>
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.department}
+                </td>
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.major}
+                </td>
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.schedule}
+                </td>
+                <td className="py-2 px-4 border-b whitespace-nowrap">
+                  {subject.enrollment} 名
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

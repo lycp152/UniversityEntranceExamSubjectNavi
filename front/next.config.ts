13e651d7a321314e861dd840f1ebb2c166636486
lycp152 <118104209+lycp152@users.noreply.github.com>
2025-04-12 18:43:12 +0900

    fix: リンターのエラーを修正

--- front/next.config.ts
+++ front/next.config.ts
@@ -8,6 +8,11 @@
  */
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  typescript: {
+    // ビルド時の型チェックを有効化
+    ignoreBuildErrors: false,
+  },
+};
 
 export default nextConfig;

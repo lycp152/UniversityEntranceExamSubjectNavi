a9d9f38a7fc0a7f3182848c1b318e37424d00013
lycp152 <118104209+lycp152@users.noreply.github.com>
2025-04-17 19:44:16 +0900

    fix: back/tests/unit/benchmarks_test.goのリンターエラーを修正

--- back/tests/unit/benchmarks_test.go
+++ back/tests/unit/benchmarks_test.go
@@ -79,11 +79,15 @@ func BenchmarkConcurrentUserCreation(b *testing.B) {
 	var wg sync.WaitGroup
 	for i := 0; i < b.N; i++ {
 		wg.Add(1)
-		go func(i int) {
+
+		user := users[i]
+		go func() {
 			defer wg.Done()
-			_ = repo.Create(users[i])
-		}(i)
+
+			_ = repo.Create(user)
+		}()
 	}
+
 	wg.Wait()
 }
 
